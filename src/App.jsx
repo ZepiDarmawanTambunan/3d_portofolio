71982136931dd32b3d1cc1ef31b7f346dc989a1b
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-08-06 15:26:46 +0700

    first commit and add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/logo.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl text-green-600 font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
