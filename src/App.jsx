26069feed885ef553c6128396667b5d7bf80c1d3
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-08-06 15:42:55 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/logo.svg'
-import './App.css'
 
-export default function App() {
+const App = () => {
   return (
     <h1 className="text-3xl text-green-600 font-bold underline">
       Hello world!
     </h1>
   )
 }
+
+export default App;
